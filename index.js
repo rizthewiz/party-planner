f98e276a04ecae5591b9168653513e0f4d909cbe
rizthewiz <rizthewiz>
2023-11-06 19:33:37 -0500

    added funtion to get events from API and render on page

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-const eventsApi =
-  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-acc-et-web-pt-b/events";
+const BASE_URL =
+  "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-acc-et-web-pt-b";
+const EVENTS_ENDPOINT = `${BASE_URL}/events`;
 
 const PARTY_LIST = document.getElementById("partyList");
 
-const events = [
-  {
-    id: 1199,
-    name: "Steve Party",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dui ipsum, hendrerit ut eleifend sit amet, dictum nec tortor. Ut et nunc condimentum, aliquam leo id, maximus quam. Nunc euismod venenatis dolor, sed interdum justo. Quisque in fermentum nulla. ",
-    date: "2023-12-20T10:00:00.000Z",
-    location: "20392 Cherry Street",
-    cohortId: 13,
-  },
-  {
-    id: 1199,
-    name: "Park Party ",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dui ipsum, hendrerit ut eleifend sit amet, dictum nec tortor. Ut et nunc condimentum, aliquam leo id, maximus quam. Nunc euismod venenatis dolor, sed interdum justo. Quisque in fermentum nulla. ",
-    date: "2023-12-20T10:00:00.000Z",
-    location: "20392 Cherry Street",
-    cohortId: 13,
-  },
-  {
-    id: 1199,
-    name: "Griffin Party",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dui ipsum, hendrerit ut eleifend sit amet, dictum nec tortor. Ut et nunc condimentum, aliquam leo id, maximus quam. Nunc euismod venenatis dolor, sed interdum justo. Quisque in fermentum nulla. ",
-    date: "2023-12-20T10:00:00.000Z",
-    location: "20392 Cherry Street",
-    cohortId: 13,
-  },
-];
+const events = [];
 
-function renderEvents() {
+function renderEvents(events) {
   for (let event of events) {
     const eventListItem = document.createElement("li");
     eventListItem.textContent = `Name: ${event.name} Description: ${event.description} Date: ${event.date} Location: ${event.location}`;
@@ -41,4 +14,19 @@ function renderEvents() {
   }
 }
 
-renderEvents();
+async function fetchEvents() {
+  try {
+    const response = await fetch(EVENTS_ENDPOINT);
+    if (!response.ok) {
+      console.log("API error", response.status);
+      return;
+    }
+    const jsonResponse = await response.json();
+    const events = jsonResponse.data;
+    renderEvents(events);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+fetchEvents();
